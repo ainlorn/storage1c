2865f8d6b371c661717c5562cb8fc5eabe91494b
ainlorn <ap4khomov@gmail.com>
2023-05-09 18:36:05 +0500

    add path size limit

--- src/main/java/com/msp31/storage1c/common/validation/constraint/ValidPath.java
+++ src/main/java/com/msp31/storage1c/common/validation/constraint/ValidPath.java
@@ -3,6 +3,7 @@ package com.msp31.storage1c.common.validation.constraint;
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
 import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -11,6 +12,7 @@ import java.lang.annotation.Target;
 import static java.lang.annotation.ElementType.*;
 
 @Pattern(regexp = "^[a-zA-Z0-9а-яА-Я /._\\-%@^;,'+=()\\[\\]{}&$#№—–]+$")
+@Size(min = 1, max = 255)
 @Constraint(validatedBy = {})
 @Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE })
 @Retention(RetentionPolicy.RUNTIME)
